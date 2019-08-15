9b96336a5e1ee8438e21dbd9a2d80e94028f86d9
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-08-15 17:48:19 -0400

    setup useApplicationData file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,100 +1,100 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DayList from "components/DayList";
-import InterviewerList from "components/InterviewerList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import axios from "axios";
 import {
   getAppointmentsForDay,
   getInterview,
   getInterviewersForDay
 } from "../helpers/selectors";
+import useApplicationData from "../hooks/useApplicationData";
 
 export default function Application(props) {
   //const [day, setDay] = useState("Monday");
   //const [days, setDays] = useState([]);
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewer: {}
-  });
+  //
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData();
 
   // const appointments = [getAppointmentsForDay()];
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
-  const setAppointments = appointments => setState({ ...state, appointments });
+  // const setDay = day => setState({ ...state, day });
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setAppointments = appointments => setState({ ...state, appointments });
+
+  // // const bookInterview = (id, interview) => {
+  // //   console.log(id, interview);
+
+  // //   const appointments = {
+  // //     ...state.appointments,
+  // //     [id]: { ...state.appointments[id], interview: interview }
+  // //   };
+
+  // //   console.log(state.appointments);
+  // //   console.log("appointments: ", appointments);
+
+  // //   return axios
+  // //     .put(`http://localhost:3001/api/appointments/${id}`, appointments[id])
+  // //     .then(res => {
+  // //       setState({ ...state, appointments: appointments });
+  // //     });
+  // // };
 
   // const bookInterview = (id, interview) => {
   //   console.log(id, interview);
+  //   console.log("state.appointments[id]: ", state.appointments[id]);
+
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: { ...interview }
+  //   };
+
+  //   console.log("appointment: ", appointment);
 
   //   const appointments = {
   //     ...state.appointments,
-  //     [id]: { ...state.appointments[id], interview: interview }
+  //     [id]: appointment
   //   };
 
-  //   console.log(state.appointments);
-  //   console.log("appointments: ", appointments);
-
   //   return axios
-  //     .put(`http://localhost:3001/api/appointments/${id}`, appointments[id])
-  //     .then(res => {
-  //       setState({ ...state, appointments: appointments });
+  //     .put(`/api/appointments/${id}`, {
+  //       interview: appointment.interview
+  //     })
+  //     .then(resp => {
+  //       if (!resp.status === 204) {
+  //         console.error("Server responded with a non 2xx response", resp.body);
+  //       }
+  //       setState({ ...state, appointments });
   //     });
   // };
 
-  const bookInterview = (id, interview) => {
-    console.log(id, interview);
-    console.log("state.appointments[id]: ", state.appointments[id]);
+  // const deleteInterview = id => {
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: null
+  //   };
 
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
 
-    console.log("appointment: ", appointment);
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .put(`/api/appointments/${id}`, {
-        interview: appointment.interview
-      })
-      .then(resp => {
-        if (!resp.status === 204) {
-          console.error("Server responded with a non 2xx response", resp.body);
-        }
-        setState({ ...state, appointments });
-      });
-  };
-
-  const deleteInterview = id => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .delete(`/api/appointments/${id}`, {
-        interview: appointment.interview
-      })
-      .then(resp => {
-        if (!resp.status === 204) {
-          console.error("Server responded with a non 2xx response", resp.body);
-          return;
-        }
-        setState({ ...state, appointments });
-      });
-  };
+  //   return axios
+  //     .delete(`/api/appointments/${id}`, {
+  //       interview: appointment.interview
+  //     })
+  //     .then(resp => {
+  //       if (!resp.status === 204) {
+  //         console.error("Server responded with a non 2xx response", resp.body);
+  //         return;
+  //       }
+  //       setState({ ...state, appointments });
+  //     });
+  // };
 
   const editInterview = () => {
     console.log("editInterview");
@@ -121,24 +121,24 @@ export default function Application(props) {
       );
     }
   );
-  //debugger
+  // //debugger
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:3001/api/days"),
-      axios.get("http://localhost:3001/api/appointments"),
-      axios.get("http://localhost:3001/api/interviewers")
-    ]).then(all => {
-      setState(oldState => {
-        return {
-          ...oldState, // just dealing with the things that are changing below.  Everything else isn't considered.
-          appointments: all[1].data,
-          days: all[0].data,
-          interviewers: all[2].data
-        };
-      });
-    });
-  }, []);
+  // useEffect(() => {
+  //   Promise.all([
+  //     axios.get("http://localhost:3001/api/days"),
+  //     axios.get("http://localhost:3001/api/appointments"),
+  //     axios.get("http://localhost:3001/api/interviewers")
+  //   ]).then(all => {
+  //     setState(oldState => {
+  //       return {
+  //         ...oldState, // just dealing with the things that are changing below.  Everything else isn't considered.
+  //         appointments: all[1].data,
+  //         days: all[0].data,
+  //         interviewers: all[2].data
+  //       };
+  //     });
+  //   });
+  // }, []);
 
   return (
     <main className="layout">
